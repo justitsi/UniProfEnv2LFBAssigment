4f5b0074e799867f4e42a7571cf762270d292c99
Hristo Belchev <k1804774@kingston.ac.uk>
2020-03-13 14:07:45 +0000

    some update

--- app.py
+++ app.py
@@ -13,11 +13,13 @@ from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt; plt.rcdefaults()
 
 data = pd.read_pickle('Excel_data')
-# print (data.dtypes)
-
+#print (data.dtypes)
+#print 
 data_fire = data [data['IncidentGroup']=='Fire']
 data_ss = data [data['IncidentGroup']=='Special Service']
 data_fa= data [data['IncidentGroup']=='False Alarm']
+
+#print(data_fa,data_ss,data_fire)
 
 data_tmp = data_ss.pivot_table(index=['SpecialServiceType'], aggfunc='size')
 data_tmp.to_frame()
@@ -62,12 +64,12 @@ data_tmp = data_WM.pivot_table(index=['SpecialServiceType'], aggfunc='size')
 data_tmp.to_frame()
 indexNamesArr = data_tmp.index.values
 for i in indexNamesArr:
-    print ("----------------------------------------")
-    print (i)
+    # print ("----------------------------------------")
+    # print (i)
     tmp = data_WM[data_WM['SpecialServiceType']==i]
     tmp = tmp['DateOfCall'].dt.dayofweek
     count = tmp.value_counts()
-    print (count)
+    # print (count)
 
     # TOO MANY CHARTSS
     # myfigure = plt.figure()
